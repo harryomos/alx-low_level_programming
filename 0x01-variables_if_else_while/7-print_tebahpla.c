5d45e26ad9270bda9df81d094666166024df3e2a
harryomos <esedharry@gmail.com>
2022-07-01 17:55:12 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -> assign a random number to the variable n each tim it is executed
+ * and print the last digit of the number stored in the variables n
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch - 'z'; ch >= 'a'; ch--)
+	{
+
+		putchar(ch);
+	}
+
+	putchar (10);/*this is an ascii code for new line*/
+
+	return (0);
+}
