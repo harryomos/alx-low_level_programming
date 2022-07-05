7b9cefd4276a16d10394a6b3e5a0b2bd2b9f272b
harryomos <esedharry@gmail.com>
2022-07-05 13:54:09 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,10 @@
 
 /**
  * islower -> ckecks if the character is lowercase
+ * @c: a character argument
  * Return: return 1 and 0 depending on condition
  */
 int _islower(int c)
 {
-	return (c >= 'a'; && c <= 'z';)
+	return (c >= 'a'; && c <= 'z';);
 }
