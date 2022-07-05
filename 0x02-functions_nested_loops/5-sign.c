061f51df50a090d86772b354ae91bf73e82095c6
harryomos <esedharry@gmail.com>
2022-07-05 14:56:28 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
 
+/**
+ * print_sign -> prints sign based on condition
+ * @n: argument passed
+ * return: 1, 0 -1
+ */
 int print_sign(int n)
 {
 	if (n > 0)
@@ -18,4 +23,4 @@ int print_sign(int n)
 		_putchar('-');
 		tetuurn (-1);
 	}
-
+}
