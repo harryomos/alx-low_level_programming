389dacf2f94566d08196a62d28f420d48bc12274
harryomos <esedharry@gmail.com>
2022-07-05 18:19:13 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,14 +8,14 @@ void jack_bauer(void)
 
 	for (i = 0; i < 24; i++)
 	{
-		for (j = 0; j < 50; j++)
+		for (j = 0; j < 60; j++)
 		{
 			if (i < 10)
 			{
 				_putchar('0');
 				_putchar(i + '0');
 			}
-			else if(i >= 10)
+			else if (i >= 10)
 			{
 				_putchar((i / 10) + '0');
 				_putchar((i % 10) + '0');
@@ -23,14 +23,14 @@ void jack_bauer(void)
 			if (j <= 10)
 			{
 				_putchar(':');
-			        _putchar('0');
-			        _putchar(j + '0');
+				_putchar('0');
+				_putchar(j + '0');
 			}
 			else if (j > 10)
 			{
 				_putchar':'i);
-		                _putchar((j / 10) + '0');
-		                _putchar((j % 10) + '0');
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
 			}
 			_putchar('\n');
 		}
