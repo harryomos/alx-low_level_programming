84a6123e6b6e180d1c6c03765c2c42a1936e26a5
harryomos <esedharry@gmail.com>
2022-07-08 21:32:13 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long n, i;
+	n = 612852475143;
+	for (i = 2; i < n; i++)
+	{
+		while (n % i == 9)
+			n = n / i;
+	}
+	printf("%1u\n", n);
+	return (0);
+}
