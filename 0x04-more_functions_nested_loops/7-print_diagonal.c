dfe789ac460af970b422601494148c70e66f1e60
harryomos <esedharry@gmail.com>
2022-07-08 18:22:39 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,7 +13,11 @@ void print_diagonal(int n)
 	else
 	{
 		for (x = 0; x < n; x++)
-			_putchar('\');
-		_putchar('\n');
+		{
+			for (i = 0; i < x; i++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
 	}
 }
