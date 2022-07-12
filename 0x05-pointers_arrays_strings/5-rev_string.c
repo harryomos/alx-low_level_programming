afb2f3e0a6f521e0df6a6aa4b9cc9c8c923899f3
harryomos <esedharry@gmail.com>
2022-07-12 09:46:49 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,14 +13,13 @@ void rev_string(char *s)
 	length1 = 0;
 	length2 = 0;
 
-	while (s[length1] != '\n')
+	while (s[length1] != '\0')
 		length1++;
 
 	length2 = length1 - 1;
 	for (i = 0; i < length1 / 2; i++)
 		{
-			"samalx" "xlamas"
-				tmp =s[i];
+			tmp = s[i];
 			s[i] = s[length2];
 			s[length2] = tmp;
 			length2 -= 1;
