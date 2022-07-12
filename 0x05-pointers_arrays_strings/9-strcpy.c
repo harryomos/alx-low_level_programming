122a7924efbfa2cc3a47d54731d58c4bdd32232e
harryomos <esedharry@gmail.com>
2022-07-12 14:07:39 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy -> copiar of certain number of the variable
+ * @dest:
+ * this the destiny
+ * @src: this is the copia
+ * Return: this is return copy
+ */
+char *_strcpy(char *dest. char *src)
+{
+	char *start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
